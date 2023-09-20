4b7ee2892aa8db4761b7ff720797b2d5d6d7fe92
riccardo-giuri <richicryst@gmail.com>
2023-09-20 14:41:33 +0200

    first commit with basic file setup

---
+++ movie.php
@@ -0,0 +1,19 @@
+<?php
+class Movie {
+    public $director;
+    public $title;
+    public $releaseYear;
+    public $minutes;
+    public $genre;
+
+    public function __construct(string $_title, string $_director, int $_releaseYear, int $_minutes, string $_genre) {
+        $this->title = $_title;
+        $this->director = $_director;
+        $this->releaseYear = $_releaseYear;
+        $this->genre = $_genre;
+        $this->minutes = $_minutes;
+    }
+
+    
+}
+?>
